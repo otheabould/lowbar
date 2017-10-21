0dccbf190281a45c664b562fc625da538c614592
Oliver Theabould <o.theabould@googlemail.com>
2017-10-21 21:39:20 +0100

    [identity] returns the value passed as an argument

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-describe('', () => {
-  it('', () => {
-    expect().to.equal();
-  });
-});
+const _ = {};
+
+_.identity = function(value) {
+  return value;
+};
+
+module.exports = _;
---
+++ spec/index.spec.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const _ = require('../');
+
+describe('_', () => {
+  describe('#identity', () => {
+    it('returns the argument passed to the function', () => {
+      const obj = {a: 1, b: 2, c: 3};
+      const arr = [1, 2, 3]
+      expect(_.identity(1)).to.equal(1);
+      expect(_.identity('hello')).to.equal('hello');
+      expect(_.identity(true)).to.equal(true);
+      expect(_.identity(obj)).to.equal(obj);
+      expect(_.identity(arr)).to.equal(arr);
+    });
+    it('returns the first argument passed', () => {
+      expect(_.identity(1, 2, 3, 4)).to.equal(1);
+    });
+    it('returns undefined when no value is passed', () => {
+      expect(_.identity()).to.equal(undefined);
+    });
+  });
+});

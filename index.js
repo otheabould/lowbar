770ff4c38700c6b7c40edcce920afb942960dd1e
Oliver Theabould <o.theabould@googlemail.com>
2017-10-21 21:51:00 +0100

    [first] returns the first n number of array items

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ _.identity = function(value) {
   return value;
 };
 
+_.first = function(list, n) {
+  return list.slice(0, n);
+}
+
 module.exports = _;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -8,7 +8,7 @@ describe('_', () => {
   describe('#identity', () => {
     it('returns the argument passed to the function', () => {
       const obj = {a: 1, b: 2, c: 3};
-      const arr = [1, 2, 3]
+      const arr = [1, 2, 3];
       expect(_.identity(1)).to.equal(1);
       expect(_.identity('hello')).to.equal('hello');
       expect(_.identity(true)).to.equal(true);
@@ -20,6 +20,11 @@ describe('_', () => {
     });
     it('returns undefined when no value is passed', () => {
       expect(_.identity()).to.equal(undefined);
+    });
+  });
+  describe('#first', () => {
+    it('returns the first x number of list items', () => {
+      expect(_.first([1, 2, 3, 4], 3)).to.eql([1, 2, 3]);
     });
   });
 });

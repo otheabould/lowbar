e084fcb6b05f00328f5091fd358bad9ace97c932
Oliver Theabould <o.theabould@googlemail.com>
2017-10-21 22:53:17 +0100

    [last]  returns the last item of an array/string, when n is not passed.

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ _.identity = function(value) {
 
 _.first = function(list, n) {
   if (list && !n) return list[0];
-  return typeof list === 'string' 
-    ? list.split('').slice(0, n)
-    : Array.isArray(list)
-      ? list.slice(0, n)
-      : undefined;
+  return Array.isArray(list) return list.slice(0, n);
+  if (typeof list === 'string') return list.split('').slice(0, n);
+};
+
+_.last = function(list, n) {
+  if (list && !n) return list[list.length - 1];
+  if (Array.isArray(list)) return list.slice(- n);
+  if (typeof list === 'string') return list.split('').slice(- n);
 };
 
 module.exports = _;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -41,4 +41,23 @@ describe('_', () => {
       expect(_.first('hi')).to.equal('h');
     });
   });
+  describe('#last', () => {
+    it('returns the first x number of list items', () => {
+      expect(_.last([1, 2, 3, 4], 3)).to.eql([2, 3, 4]);
+    });
+    it('returns the last x number of string characters', () => {
+      expect(_.last(('hello'), 3)).to.eql(['l', 'l', 'o']);
+    });
+    it('returns undefined for invalid arguments', () => {
+      expect(_.last({1: 2})).to.equal(undefined);
+      expect(_.last(9)).to.equal(undefined);
+      expect(_.last(2)).to.equal(undefined);
+      expect(_.last()).to.equal(undefined);
+      expect(_.last(true)).to.equal(undefined);
+    });
+    it('returns the last item of an array/string, when n is not passed.', () => {
+      expect(_.last([1, 2, 3])).to.equal(3);
+      expect(_.last('hi')).to.equal('i');
+    });
+  });
 });

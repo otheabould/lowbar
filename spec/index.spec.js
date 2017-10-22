6e2841b909e1f369b2276c15660a0bad2041f537
Oliver Theabould <o.theabould@googlemail.com>
2017-10-22 12:08:43 +0100

    [index] retuns -1 for invalid input

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -89,6 +89,16 @@ describe('_', () => {
     it('returns the first index of the passed value', () => {
       expect(_.index([1, 2, 6, 5], 6)).to.equal(2);
       expect(_.index([1, 2, 6, 6], 6)).to.equal(2);
-    })
-  })
+    });
+    it('retuns -1 if the value is not present', () => {
+      expect(_.index([1, 2, 3, 4], 7)).to.equal(-1);
+    });
+    it('retuns -1 for invalid arguments', () => {
+      expect(_.index([1, 2, 3, 4])).to.equal(-1);
+      expect(_.index({ a: 1, b: 2, c: 3, d: 4 }, 3)).to.equal(-1);
+      expect(_.index(456, 5)).to.equal(-1);
+      expect(_.index(true, 'u')).to.equal(-1);
+      expect(_.index()).to.equal(-1);
+    });
+  });
 });

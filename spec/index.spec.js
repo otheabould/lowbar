028f45a106ddb33b7c5734e0c9e1d925749597d4
Oliver Theabould <o.theabould@googlemail.com>
2017-10-25 22:04:13 +0100

    [shuffle] returns a shuffled array version of object values

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -490,16 +490,21 @@ describe('_', () => {
   });
   describe('#shuffle', () => {
     it('returns a shuffled copy of the array', () => {
-      const list = [1, 2, 3, 4];
+      const list = [1, 2, 3, 4, 5, 6];
       const shuffled = _.shuffle(list);
       expect(shuffled.length).to.equal(list.length);
       expect(shuffled).to.not.eql(list);
     });
-    it('returns a shuffled copy of the a string in an array', () => {
-      const list = '1234';
+    it('returns a shuffled copy of the string in an array', () => {
+      const list = '123456';
       const shuffled = _.shuffle(list);
-      console.log(shuffled);
       expect(shuffled.length).to.equal(list.length);
+      expect(shuffled).to.not.eql(list);
+    });
+    it('returns a shuffled copy of the object values in an array', () => {
+      const list = {a: 1, b: 2, c: 3, d: 4, e: 5};
+      const shuffled = _.shuffle(list);
+      expect(shuffled.length).to.equal(Object.values(list).length);
       expect(shuffled).to.not.eql(list);
     });
   });

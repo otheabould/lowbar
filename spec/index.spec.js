72fd4205d789dcbdebd9e4503aeaf9fc28020726
Oliver Theabould <o.theabould@googlemail.com>
2017-10-28 21:47:46 +0100

    [zip] can be used on nested arrays when invoked with apply

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -587,5 +587,18 @@ describe('_', () => {
       const expected = [['moe', 30], ['larry', 40], ['curly', 50]];
       expect(actual).to.eql(expected);
     });
+    it('iterates through each item in every array', () => {
+      const list1 = ['moe', 'larry', 'curly'];
+      const list2 = [30, 40, 50];
+      const actual = _.zip(list1, list2);      
+      const expected = [['moe', 30], ['larry', 40], ['curly', 50]];
+      expect(actual).to.eql(expected);
+    });
+    it('works on nested arrays when called with apply', () => {
+      const list = [['moe', 'larry', 'curly'], [30, 40, 50]];
+      const actual = _.zip.apply(null, list);      
+      const expected = [['moe', 30], ['larry', 40], ['curly', 50]];
+      expect(actual).to.eql(expected);
+    });
   });
 });

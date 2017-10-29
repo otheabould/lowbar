68f987dae0ea069284d5942a574a24d2f95eb970
Oliver Theabould <o.theabould@googlemail.com>
2017-10-29 16:40:53 +0000

    [difference] check against any number of other arrays

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -679,9 +679,18 @@ describe('_', () => {
   describe('#difference', () => {
     it('returns the values from the array that are not present in the others', () => {
       const arr = [1, 2, 3, 4, 5];
-      const others = [5, 2, 10];
-      const actual = _.difference(arr, others);
+      const other1 = [5, 2, 10];
+      const actual = _.difference(arr, other1);
       const expected = [1, 3, 4];
+      expect(actual).to.eql(expected);
+    });
+    it('check against any number of other arrays', () => {
+      const arr = [1, 2, 3, 4, 5, 7, 8, 9];
+      const other1 = [5, 2, 10];
+      const other2 = [5, 7];
+      const other3 = [9, 3];
+      const actual = _.difference(arr, other1, other2, other3);
+      const expected = [1, 4, 8];
       expect(actual).to.eql(expected);
     });
   });
